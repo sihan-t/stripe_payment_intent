f1db319138eec4d7e315f4a7456775815b7cef47
sihan-tawsik <sihantawsik@gmail.com>
2023-05-03 15:23:54 +0600

    Add stripe apis

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+    apiVersion: "2022-11-15",
+});
+
+app.post("/test", async (req: Request, res: Response) => {
+    let customer = null;
+    let customers = (
+        await stripe.customers.search({ query: `email:"${req.body.email}"` })
+    ).data;
+    if (customers.length === 0) {
+        customer = await stripe.customers.create({
+            email: req.body.email,
+            source: "tok_visa",
+        });
+    } else {
+        customer = customers[0];
+    }
+    res.status(200).json(customer);
+});
 
 app.listen(8000, () => {
-    console.log("App running on 8000");
+    console.log("App running on port 8000");
 });
